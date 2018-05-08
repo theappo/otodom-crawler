aba6fc7ce8fbcfaedd036eed315d8da9c1ea5766
pixinixi <pw.pixie@gmail.com>
2018-05-08 18:13:46 +0200

    Update format.py

--- scrapper/format.py
+++ scrapper/format.py
@@ -15,87 +15,84 @@ read_file = file.read()
 
 lista_mieszkania_zmienne = []
 
-lista_mieszkan = [read_file.split("_!_!_")]
+lista_mieszkan = read_file.split("_!_!_")
 
 podzielona_lista_mieszkan = []
 
 for mieszkanie in lista_mieszkan:
-    for x in mieszkanie:
-        koniec_tytulu = x.find("dzielnicaadres:", 1)
-        x = x[koniec_tytulu:]
-        podzielona_lista_mieszkan.append([x])
-
+    koniec_tytulu = mieszkanie.find("dzielnicaadres:", 1)
+    mieszkanie = mieszkanie[koniec_tytulu:]
+    podzielona_lista_mieszkan.append(mieszkanie)
 
 for mieszkanie in podzielona_lista_mieszkan:
-    for i in mieszkanie:
 
-        id = i.find('Nr oferty w Otodom:') + 20
-        id_koniec = id + 8
-        id_oferty = i[id:id_koniec]
+    id = mieszkanie.find('Nr oferty w Otodom:') + 20
+    id_koniec = id + 8
+    id_oferty = mieszkanie[id:id_koniec]
 
-        cena = i.find('Cena') + 5
-        cena_koniec = i.find('zł') - 1
-        powierzchnia = i.find('Powierzchnia') + 13
-        powierzchnia_koniec = i.find('Liczba pokoi') - 4
-        liczba_pokoi = i.find('Liczba pokoi') + 13
-        liczba_pokoi_koniec = liczba_pokoi + 1
+    cena = mieszkanie.find('Cena') + 5
+    cena_koniec = mieszkanie.find('zł') - 1
+    powierzchnia = mieszkanie.find('Powierzchnia') + 13
+    powierzchnia_koniec = mieszkanie.find('Liczba pokoi') - 4
+    liczba_pokoi = mieszkanie.find('Liczba pokoi') + 13
+    liczba_pokoi_koniec = liczba_pokoi + 1
 
 
-        adres1 = i.find('dzielnicaadres:') + 39
-        adres1_koniec = i.find('- Zobacz na mapie')
+    adres1 = mieszkanie.find('dzielnicaadres:') + 39
+    adres1_koniec = mieszkanie.find('- Zobacz na mapie')
 
-        lista_mieszkania_zmienne.append(id_oferty)
-        price = i[cena:cena_koniec]
-        lista_mieszkania_zmienne.append(price)
-        size = i[powierzchnia:powierzchnia_koniec]
-        lista_mieszkania_zmienne.append(size)
-        rooms = i[liczba_pokoi:liczba_pokoi_koniec]
-        lista_mieszkania_zmienne.append(rooms)
+    lista_mieszkania_zmienne.append(id_oferty)
+    price = mieszkanie[cena:cena_koniec]
+    lista_mieszkania_zmienne.append(price)
+    size = mieszkanie[powierzchnia:powierzchnia_koniec]
+    lista_mieszkania_zmienne.append(size)
+    rooms = mieszkanie[liczba_pokoi:liczba_pokoi_koniec]
+    lista_mieszkania_zmienne.append(rooms)
 
-        pi = "Piętro"
-        if pi in i:
-            pietro = i.find('Piętro') + 7
-            pietro_koniec = pietro + 1
-            pietra = i[pietro:pietro_koniec]
-            lista_mieszkania_zmienne.append(pietra)
+    pi = "Piętro"
+    if pi in mieszkanie:
+        pietro = mieszkanie.find('Piętro') + 7
+        pietro_koniec = pietro + 1
+        pietra = mieszkanie[pietro:pietro_koniec]
+        lista_mieszkania_zmienne.append(pietra)
+    else:
+        lista_mieszkania_zmienne.append("NA")
+
+    adres = mieszkanie[adres1:adres1_koniec]
+    lista_mieszkania_zmienne.append(adres)
+
+    longitude = mieszkanie.find('data-lat=') + 10
+    latitude = mieszkanie.find('data-lon=') + 10
+    longitude_koniec = latitude - 12
+    latitude_koniec = mieszkanie.find('data-poi-lat=') - 2
+
+    dlugosc = mieszkanie[longitude:longitude_koniec]
+    lista_mieszkania_zmienne.append(dlugosc)
+    szerokosc = mieszkanie[latitude:latitude_koniec]
+    lista_mieszkania_zmienne.append(szerokosc)
+
+    cat_var = ("Rynek:", "Rodzaj zabudowy:", "Materiał budynku:", "Okna:", "Ogrzewanie:",
+               "Rok budowy:", "Stan wykończenia: do", "Czynsz:", "Forma własności:")
+
+    for var in cat_var:
+        regex = var + r" (\w+)"
+        match = re.search(regex, mieszkanie)
+        if match:
+            lista_mieszkania_zmienne.append(match.group(1))
         else:
-            lista_mieszkania_zmienne.append("NA")
+            lista_mieszkania_zmienne.append("0")
 
-        adres = i[adres1:adres1_koniec]
-        lista_mieszkania_zmienne.append(adres)
+    var01 = ("internet", "telewizja kablowa", "telefon", "rolety antywłamaniowe", "drzwi / okna antywłamaniowe",
+             "domofon / wideofon","monitoring / ochrona", "system alarmowy", "teren zamknięty", "meble", "pralka",
+             "zmywarka", "lodówka", "kuchenka", "piekarnik", "telewizor", "balkon", "pom. użytkowe",
+             "garaż/miejsce parkingowe", "piwnica", "ogródek", "taras", "winda", "dwupoziomowe", "oddzielna kuchnia",
+             "klimatyzacja")
 
-        longitude = i.find('data-lat=') + 10
-        latitude = i.find('data-lon=') + 10
-        longitude_koniec = latitude - 12
-        latitude_koniec = i.find('data-poi-lat=') - 2
-
-        dlugosc = i[longitude:longitude_koniec]
-        lista_mieszkania_zmienne.append(dlugosc)
-        szerokosc = i[latitude:latitude_koniec]
-        lista_mieszkania_zmienne.append(szerokosc)
-
-        cat_var = ("Rynek:", "Rodzaj zabudowy:", "Materiał budynku:", "Okna:", "Ogrzewanie:",
-                    "Rok budowy:", "Stan wykończenia: do", "Czynsz:", "Forma własności:")
-
-        for var in cat_var:
-            regex = var + r" (\w+)"
-            match = re.search(regex, i)
-            if match:
-                lista_mieszkania_zmienne.append(match.group(1))
-            else:
-                lista_mieszkania_zmienne.append("0")
-
-        var01 = ("internet", "telewizja kablowa", "telefon", "rolety antywłamaniowe", "drzwi / okna antywłamaniowe",
-                 "domofon / wideofon","monitoring / ochrona", "system alarmowy", "teren zamknięty", "meble", "pralka",
-                 "zmywarka", "lodówka", "kuchenka", "piekarnik", "telewizor", "balkon", "pom. użytkowe",
-                 "garaż/miejsce parkingowe", "piwnica", "ogródek", "taras", "winda", "dwupoziomowe", "oddzielna kuchnia",
-                 "klimatyzacja")
-
-        for var in var01:
-            if var in i:
-                lista_mieszkania_zmienne.append("1")
-            else:
-                lista_mieszkania_zmienne.append("0")
+    for var in var01:
+        if var in mieszkanie:
+            lista_mieszkania_zmienne.append("1")
+        else:
+            lista_mieszkania_zmienne.append("0")
 
 lista_do_csv = [lista_mieszkania_zmienne[x:x + 43] for x in range(0, len(lista_mieszkania_zmienne), 43)]
 
